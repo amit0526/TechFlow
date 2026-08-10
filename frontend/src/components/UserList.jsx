16ddc4bf8f1bcdd3dd175460129f044596306099
Amit Anand <amit.anand0526@gmail.com>
2026-08-11 02:33:43 +0530

    Add delete button

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,12 +1,18 @@
-function UserList({ users }) {
+function UserList({ users ,deleteUser }) {
   return (
     <div className="space-y-4">
       {users.map((user) => (
-        <div key={user.id} className="bg-slate-800 p-5 rounded-xl">
+        <div key={user.id} className="bg-slate-800 p-5 rounded-xl flex items-center justify-between">
+          <div>
           <h2 className="text-xl font-semibold">{user.name}</h2>
 
           <p className="text-slate-400">{user.email}</p>
-        </div>
+          </div>
+          <button
+            onclick={() => deleteUser(user.id)}
+            className="bg-red-500 hover:bg-red-400 text-white font-semibold px-4 py-2 rounded-lg"> Delete
+          </button>
+          </div>
       ))}
     </div>
   );
